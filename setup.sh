e8804354cc0e6237db17e9dcb3ed8e8521e39686
zacheryvaughn <zacvaughn.dev@gmail.com>
2025-04-06 19:05:39 +0800

    updated install requirements

--- setup.sh
+++ setup.sh
@@ -2,5 +2,5 @@ python3 -m venv .venv && \
 source .venv/bin/activate && \
 pip install --upgrade pip && \
 pip cache purge && \
-pip install torch torchvision diffusers transformers && \
+pip install torch torchvision diffusers transformers noise opencv-python && \
 pip freeze > requirements.txt
